f94e3cbc6dbbced541ddcf088ceded192139873d
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-02-28 22:23:54 -0600

    Add the app bar exercise (10.4) - Add theme.js for consistent theming - Add Text component w/ theme styling - Update RepositoryItem to use new Text component - Add Subheading component - Add AppBarTab component - Add AppBar component - Add AppBar component to Main component

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    textAppBar: '#fff',
+    primary: '#0366d6',
+    bgPrimary: '#fff',
+    bgAppBar: '#24292e',
+    separator: '#e1e5e8',
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+  },
+  fonts: {
+    main: 'System',
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+};
+
+export default theme;
